a6a7ad3f022a29c3a21672f24a8f0b4518653003
nekowawolf <irukainu20@gmail.com>
2024-05-31 18:03:24 +0700

    update : get3

--- js/controller/post.js
+++ js/controller/post.js
@@ -1,6 +1,6 @@
 import { postData } from "https://bukulapak.github.io/api/process.js";
 import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
-import { urlPOST, AmbilResponse} from "../config/url_post.js";
+import { urlPOST, AmbilResponse} from "/js/controller/url_post";
 
 
 function pushData(){
